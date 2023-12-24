99a660d42e509cade10150710ac039e517597c40
Cicero Sousa <cicerosousa.le@gmail.com>
2023-12-24 15:56:03 -0300

    Atualização 7

---
+++ Areas/Contatos/Models/Services/ContatoService.cs
@@ -0,0 +1,25 @@
+﻿using AgendaDio.Shared.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AgendaDio.Areas.Contatos.Models.Services
+{
+    public class ContatoService : IContaoService
+    {
+        private readonly IGenericRepository<Contato> _contatoRepository;
+
+        public ContatoService(IGenericRepository<Contato> contatoRepository)
+        {
+            _contatoRepository = contatoRepository;
+        }
+        public async Task<bool> Adicionar(Contato contato, CancellationToken cancellationToken)
+        {
+            await _contatoRepository.AddAsync(contato, cancellationToken).ConfigureAwait(false);
+            var result = await _contatoRepository.CommitAsync(cancellationToken).ConfigureAwait(false);
+            return result;
+        }
+    }
+}
---
+++ Areas/Contatos/Models/Services/IContaoService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AgendaDio.Areas.Contatos.Models.Services
+{
+    public interface IContaoService
+    {
+        Task<bool> Adicionar(Contato contato, CancellationToken cancellationToken);
+    }
+}
